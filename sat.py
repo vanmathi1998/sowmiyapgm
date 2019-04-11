fed1d16fbfcb979316cac5e197300d64cdea3bc9
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-04-11 12:23:52 +0530

    sat.py

--- sat.py
+++ sat.py
@@ -1,7 +1,7 @@
-num=float(input("enter a number:"))
-if num>0:
+n=float(input("enter a number:"))
+if n>0:
 print("positive number")
-elif num==0:
+elif n==0:
 print("zero")
 else:
 print("negative number")
