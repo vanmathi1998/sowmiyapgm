ed52267ce8e25e9bdef6554d5f12ecac7a50da8a
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 10:42:10 +0530

    36.py

---
+++ 36.py
@@ -0,0 +1,12 @@
+a=input('enter the string')
+s=d=spec=0
+for i in range(len(a)):
+if(a[i].ias()):
+s=s+1
+elif(a[i].iad()):
+d=d+1
+else:
+spcl=spcl+1
+print('/ntotal number of alphabets are:',s)
+print('/ntotal number of digits  are:',d)
+print('/n total number of special character are:''spcl)
