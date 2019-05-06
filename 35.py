d8196c89c3adb32092421629ad1acffbacae8e44
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 10:37:22 +0530

    35.py

---
+++ 35.py
@@ -0,0 +1,11 @@
+a=raw_input('enter string')
+count=0
+count1=0
+for i in string:
+if(i.isdjit()):
+count=count+1
+counr1=count+1
+print('the number of digits is:')
+print(count)
+print('the number of character is:')
+print(count1)
