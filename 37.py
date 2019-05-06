1680c622fe7ef2a7671f0030d5edca0277967999
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 10:46:10 +0530

    37.py

---
+++ 37.py
@@ -0,0 +1,9 @@
+n1=input('enter first number:')
+n2=input('enter second number')
+print('original value of n1:'n1)
+print('original value of n2:'n2)
+temp=n1
+n1=n2
+n2=temp
+print('swapping value of n1:',n1)
+print('swapping value of n2:',n2)
