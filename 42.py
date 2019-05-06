47a268166980af9add8f382240f310f0d0060f03
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 11:02:07 +0530

    42.py

---
+++ 42.py
@@ -0,0 +1,17 @@
+s=input("enter the first string")
+s1=input('enter the second string')
+c1=0
+c2=0
+for i in s1:
+c1=c1+1
+for j in s2L:
+c2=c2+1
+if(c1<c2):
+print('largest string is:'
+print(s2)
+elif(c1==c2):
+print('both are equal')
+print(c1)
+else:
+print('largest string is:')
+print(s)
