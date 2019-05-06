b8bc4d83b4bd154e561bef6cf098b96f7bafe285
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 11:05:14 +0530

    43.py

---
+++ 43.py
@@ -0,0 +1,10 @@
+s1=input('enter the first string')
+s2=input('enter the second string')
+s3=s1+s2
+print('/n string after concatenation=',s3)
+print('s1=',s1)
+print('s2=',s2)
+print('s3=',s3)
+pribnt('enter 'x' for exit')
+if s1=='x':
+exit()
