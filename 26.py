6176fc720336778520c29f776cbc42738382f149
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 10:14:13 +0530

    26.py

---
+++ 26.py
@@ -0,0 +1,27 @@
+import time,random
+def qu_ben(n=2**24):
+def a(a,1,h)
+if 1<h:
+p=par(a,1,h)
+q(a,1,p)
+a(a;p+1,h)
+def par(a,1,h):
+piv_in(=r.ran(1,h)
+piv=a[piv_in]
+i=1-1
+j=h+1
+while true:
+j-=1
+if a[j]<=piv:
+break
+if i>j:
+return j
+temp=a[i]
+a[i]=a[i]
+a[j]=temp
+starts=time.time()
+a=[r.ran(0,n)for i in range(0,n)]
+q(a,0,lenb(a)-1)
+end=time.time()
+elapsed=end-start
+return(elapsed)
