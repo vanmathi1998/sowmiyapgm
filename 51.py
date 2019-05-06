cc9cf61a8aac312396d007e2c3e26604e3a7c107
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 11:44:15 +0530

    51.py

---
+++ 51.py
@@ -0,0 +1,9 @@
+while true:
+try
+number=int(input('enter three digit number:'))in range(99,1000)
+break
+except valueerror:
+print(enter a three digit number:')
+continue
+numbers=str(number)
+prinbt('yours digits are:','numbers[0],numbers[1],numbers[2])
