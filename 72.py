43b415544b49bcc8048c9407b9c87dce53be7225
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-06 16:37:52 +0530

    72.py

---
+++ 72.py
@@ -0,0 +1,14 @@
+def check(string):
+vowels=set('aeiou')
+s=set({})
+for char in string:
+if char in vowe;s:
+s.add(char)
+else:
+pass
+if len(s)==len(vowels):
+print('acccepted')'
+if__name__''__main__':
+string='seequial'
+string=string.lower
+check(string)
