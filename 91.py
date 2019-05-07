d268e3b85f027835f7e6e07b43564e5c30ba3ee1
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-07 15:57:42 +0530

    91.py

---
+++ 91.py
@@ -0,0 +1,9 @@
+def volumecuboid(1,h,w):
+return(1*h*w)
+def surfaceareacuboid(1,h,w):
+return(2*1*w+2*w*h+2*1+h)
+l=1
+h=5
+w=7
+print('volume='volumecuboid(1,h,w))
+print('total surface area=',surfaceareacuboid(1,h,w))
