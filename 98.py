8dbbf11c9b2229541d380b4b4d3f16befdc53557
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-07 16:15:05 +0530

    98.py

---
+++ 98.py
@@ -0,0 +1,10 @@
+def max(arr,l;ow,how):
+max=arr[low]
+i=low
+for i in range(high+1)
+if arr[i]>max:
+max=arr[i]
+return max
+arr=[1,30,40,50,60,70]
+n=len(arr)
+print('the maximum element is %d'%max(arr,0,n-1))
