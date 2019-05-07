501efdbdc3ffd41aab5c5c72d593fc6d69296f88
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-07 14:51:15 +0530

    73.c

---
+++ 73.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int  N,L,R,count=0;
+scanf('%d',&N);
+scanf('%d %d',&l,&R);
+for (i=L;i<R;i++)
+{
+if(N==i)
+{
+count=1;
+}
+}
+if(count==1)
+{
+print('yes')
+}
+else
+print('no');
+return0;
+}
