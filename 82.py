1a7c3f3946c62f7e27a21ab00c65f14da5c4601a
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-07 15:33:26 +0530

    82.py

---
+++ 82.py
@@ -0,0 +1,6 @@
+x=int(input('enter the number'))
+y=int(input('enter tyhe number'))
+print('\n original number:',x)
+print('formatted number with sign'+'{:+2f}'.x);
+print('original number'y)
+print(('formatted number with sign'+'{:+2f}'.y);
