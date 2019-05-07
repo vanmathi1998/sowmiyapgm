27410029107834024e78b00550e8a7fae5b922f0
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-07 14:58:01 +0530

    75.py

---
+++ 75.py
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char str[20],len,n;
+scnf('%s',&str);
+len=strlen(str);
+n=len/2'
+if(len%2)
+{
+str[n]='*';
+str[n+1]=]'';
+}
+else
+{
+str[n]='*';
+}
+print('%s',str);
+return0;
+}
