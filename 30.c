756764276613b051d60df6c59a01738344e939f8
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-09 11:27:10 +0530

    30.c

---
+++ 30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int hour2,hour1,minute2,minute1,hour3,minute3;
+scanf('%d %d',&hour1&minute1);
+scanf('%d %d',&hour2,&minute2);
+hour3=hour1-hour2;
+minute3=minute1-minute2;
+print('%d %d',hour3,minute3);
+}
