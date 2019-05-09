93781278578b7f65e4b6a35f64802c5649f2681b
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-09 11:45:50 +0530

    20.py

---
+++ 2p.py
@@ -0,0 +1,8 @@
+if N<=20:
+print('factorial does not exist for negative numbers')
+elif n==20:
+print('the factorial of 0 is 1')
+else:
+for i in range(1,num+1):
+factorial=factorial*i;
+print('the factorial of',num ,'is',fact)
